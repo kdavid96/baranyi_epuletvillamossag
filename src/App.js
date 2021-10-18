0252705c4ed904c00ac2edee4181ab818c35f702
Dávid Koppány <57870734+kdavid96@users.noreply.github.com>
2021-10-18 21:20:59 +0200

    99% overflow-mentesitett v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {Header, About, Work, Contact, Footer, MobileNav} from './components/index';
 import { useState } from 'react';
 import './App.css';
-import ScrollUp from './components/ScrollUp';
 
 function App() {
   const [isOpen, setOpen] = useState(false);
