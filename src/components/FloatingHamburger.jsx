e065d6762399a2a52627c501d023da22a33892b4
Dávid Koppány <57870734+kdavid96@users.noreply.github.com>
2021-11-03 11:21:14 +0100

    Header responsive, hamburger repositioned

--- src/components/FloatingHamburger.jsx
+++ src/components/FloatingHamburger.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function MobileNav({isOpen, setOpen}) {
     return(
-        <Fade left>
+        <Fade right>
             <div className="hamburger-div">
                 <Hamburger style={{display: 'none'}} toggled={isOpen} toggle={setOpen} color="black"/>
             </div>
